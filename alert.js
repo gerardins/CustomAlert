0ecb827664799a86b27f4eb4d37ed995f9c4a3d6
Konjoh <selabielvis@gmail.com>
2014-03-26 09:43:56 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -3,6 +3,26 @@
 	 * email selabielvis@gmail.com
 	 *
 	**/
+var confirmOnLeave = function(msg) {
+	 
+    window.onbeforeunload = function (e) {
+        e = e || window.event;
+        msg = msg || '';
+ 
+        // For IE and Firefox
+        if (e) {e.returnValue = msg;}
+ 
+        // For Chrome and Safari
+        return msg;
+    };
+ 
+};
+var cancelConfirmOnLeave = function() {
+	 
+    window.onbeforeunload = true;
+ 
+};
+
 
 var alertConfig={
 			boxBgClass:"",//Background class
@@ -15,7 +35,7 @@ var alertConfig={
  *
  */
  
-window.alert = function(alertMessage,title)// On  declare la fonction qui va redesigner l'alerte de base en testant s'il s'agit bien d'une fenêtre alert()
+window.alert = function(alertMessage,title)// On  declare la fonction qui va redesigner l'alerte de base en testant s'il s'agit bien d'une fenÃªtre alert()
 {	
 		if(title==undefined)
 		 title="Alert";
@@ -81,31 +101,24 @@ window.alert = function(alertMessage,title)// On  declare la fonction qui va red
             },100);
         }, false);
         
-        var urlToGo = null;
-        
-        if(typeof event != "undefined" && event.type == 'click'){
-            urlToGo = event.target.origin;
-            event.preventDefault();
-        } else if(typeof event != "undefined" && event.type == 'submit'){
-            urlToGo = event.target.action;
-            event.preventDefault();
-        }
-        
         // Button on click event
         _btn.addEventListener("click", function(){
             _body.removeChild(_box);
             _body.removeChild(_layer);
             if(typeof urlToGo == 'string'){
-                window.location.href = urlToGo;
+            	cancelConfirmOnLeave("");
+                //window.location.href = urlToGo;
             }
-			
+           // alert("btn close");
+            cancelConfirmOnLeave("");
+            
         }, false);
         
         // Window resize event
         window.onresize = function(event) {
             // Set box position
 			_box.setAttribute('style', 'background-color:'+_boxBgColor+';position:absolute;z-index:9999;top:'+parseInt((window.innerHeight/2)-_box.offsetHeight)+'px;left:'+parseInt((window.innerWidth/2)-(_box.offsetWidth/2))+'px;');
-        }
+        };
         
         // Document keypress event
         document.onkeypress = function(event) {
@@ -113,120 +126,127 @@ window.alert = function(alertMessage,title)// On  declare la fonction qui va red
                 _body.removeChild(_box);
                 _body.removeChild(_layer);
                 if(typeof urlToGo == 'string'){
-                    window.location.href = urlToGo;
+                    //window.location.href = urlToGo;
+                    //window.onbeforeunload = true;
+                    
                 }
+                cancelConfirmOnLeave("");
             }
-        }
+        };
         
         _close_btn.addEventListener("click",function(){
+        	
+        	
             _body.removeChild(_box);
             _body.removeChild(_layer);
-			
-        }, false);
+           // window.onbeforeunload = true;
+            cancelConfirmOnLeave("");
+        });
         
-}
+        confirmOnLeave("test");
+};
 
 function HTMLentities(texte) {
-	texte = texte.replace(/˜/g,'&tilde;'); // 152 98
-	texte = texte.replace(/™/g,'&trade;'); // 153 99
-	texte = texte.replace(/š/g,'&scaron;'); // 154 9A
-	texte = texte.replace(/›/g,'&rsaquo;'); // 155 9B
-	texte = texte.replace(/œ/g,'&oelig;'); // 156 9C
-	texte = texte.replace(/ž/g,'&#382;'); // 158 9E
-	texte = texte.replace(/Ÿ/g,'&Yuml;'); // 159 9F
-	texte = texte.replace(/¡/g,'&iexcl;'); // 161 A1
-	texte = texte.replace(/¢/g,'&cent;'); // 162 A2
-	texte = texte.replace(/£/g,'&pound;'); // 163 A3
-	texte = texte.replace(/¥/g,'&yen;'); // 165 A5
-	texte = texte.replace(/¦/g,'&brvbar;'); // 166 A6
-	texte = texte.replace(/§/g,'&sect;'); // 167 A7
-	texte = texte.replace(/¨/g,'&uml;'); // 168 A8
-	texte = texte.replace(/©/g,'&copy;'); // 169 A9
-	texte = texte.replace(/ª/g,'&ordf;'); // 170 AA
-	texte = texte.replace(/«/g,'&laquo;'); // 171 AB
-	texte = texte.replace(/¬/g,'&not;'); // 172 AC
-	texte = texte.replace(/­/g,'&shy;'); // 173 AD
-	texte = texte.replace(/®/g,'&reg;'); // 174 AE
-	texte = texte.replace(/¯/g,'&macr;'); // 175 AF
-	texte = texte.replace(/°/g,'&deg;'); // 176 B0
-	texte = texte.replace(/±/g,'&plusmn;'); // 177 B1
-	texte = texte.replace(/²/g,'&sup2;'); // 178 B2
-	texte = texte.replace(/³/g,'&sup3;'); // 179 B3
-	texte = texte.replace(/´/g,'&acute;'); // 180 B4
-	texte = texte.replace(/µ/g,'&micro;'); // 181 B5
-	texte = texte.replace(/¶/g,'&para'); // 182 B6
-	texte = texte.replace(/·/g,'&middot;'); // 183 B7
-	texte = texte.replace(/¸/g,'&cedil;'); // 184 B8
-	texte = texte.replace(/¹/g,'&sup1;'); // 185 B9
-	texte = texte.replace(/º/g,'&ordm;'); // 186 BA
-	texte = texte.replace(/»/g,'&raquo;'); // 187 BB
-	texte = texte.replace(/¼/g,'&frac14;'); // 188 BC
-	texte = texte.replace(/½/g,'&frac12;'); // 189 BD
-	texte = texte.replace(/¾/g,'&frac34;'); // 190 BE
-	texte = texte.replace(/¿/g,'&iquest;'); // 191 BF
-	texte = texte.replace(/À/g,'&Agrave;'); // 192 C0
-	texte = texte.replace(/Á/g,'&Aacute;'); // 193 C1
-	texte = texte.replace(/Â/g,'&Acirc;'); // 194 C2
-	texte = texte.replace(/Ã/g,'&Atilde;'); // 195 C3
-	texte = texte.replace(/Ä/g,'&Auml;'); // 196 C4
-	texte = texte.replace(/Å/g,'&Aring;'); // 197 C5
-	texte = texte.replace(/Æ/g,'&AElig;'); // 198 C6
-	texte = texte.replace(/Ç/g,'&Ccedil;'); // 199 C7
-	texte = texte.replace(/È/g,'&Egrave;'); // 200 C8
-	texte = texte.replace(/É/g,'&Eacute;'); // 201 C9
-	texte = texte.replace(/Ê/g,'&Ecirc;'); // 202 CA
-	texte = texte.replace(/Ë/g,'&Euml;'); // 203 CB
-	texte = texte.replace(/Ì/g,'&Igrave;'); // 204 CC
-	texte = texte.replace(/Í/g,'&Iacute;'); // 205 CD
-	texte = texte.replace(/Î/g,'&Icirc;'); // 206 CE
-	texte = texte.replace(/Ï/g,'&Iuml;'); // 207 CF
-	texte = texte.replace(/Ð/g,'&ETH;'); // 208 D0
-	texte = texte.replace(/Ñ/g,'&Ntilde;'); // 209 D1
-	texte = texte.replace(/Ò/g,'&Ograve;'); // 210 D2
-	texte = texte.replace(/Ó/g,'&Oacute;'); // 211 D3
-	texte = texte.replace(/Ô/g,'&Ocirc;'); // 212 D4
-	texte = texte.replace(/Õ/g,'&Otilde;'); // 213 D5
-	texte = texte.replace(/Ö/g,'&Ouml;'); // 214 D6
-	texte = texte.replace(/×/g,'&times;'); // 215 D7
-	texte = texte.replace(/Ø/g,'&Oslash;'); // 216 D8
-	texte = texte.replace(/Ù/g,'&Ugrave;'); // 217 D9
-	texte = texte.replace(/Ú/g,'&Uacute;'); // 218 DA
-	texte = texte.replace(/Û/g,'&Ucirc;'); // 219 DB
-	texte = texte.replace(/Ü/g,'&Uuml;'); // 220 DC
-	texte = texte.replace(/Ý/g,'&Yacute;'); // 221 DD
-	texte = texte.replace(/Þ/g,'&THORN;'); // 222 DE
-	texte = texte.replace(/ß/g,'&szlig;'); // 223 DF
-	texte = texte.replace(/à/g,'&agrave;'); // 224 E0
-	texte = texte.replace(/á/g,'&aacute;'); // 225 E1
-	texte = texte.replace(/â/g,'&acirc;'); // 226 E2
-	texte = texte.replace(/ã/g,'&atilde;'); // 227 E3
-	texte = texte.replace(/ä/g,'&auml;'); // 228 E4
-	texte = texte.replace(/å/g,'&aring;'); // 229 E5
-	texte = texte.replace(/æ/g,'&aelig;'); // 230 E6
-	texte = texte.replace(/ç/g,'&ccedil;'); // 231 E7
-	texte = texte.replace(/è/g,'&egrave;'); // 232 E8
-	texte = texte.replace(/é/g,'&eacute;'); // 233 E9
-	texte = texte.replace(/ê/g,'&ecirc;'); // 234 EA
-	texte = texte.replace(/ë/g,'&euml;'); // 235 EB
-	texte = texte.replace(/ì/g,'&igrave;'); // 236 EC
-	texte = texte.replace(/í/g,'&iacute;'); // 237 ED
-	texte = texte.replace(/î/g,'&icirc;'); // 238 EE
-	texte = texte.replace(/ï/g,'&iuml;'); // 239 EF
-	texte = texte.replace(/ð/g,'&eth;'); // 240 F0
-	texte = texte.replace(/ñ/g,'&ntilde;'); // 241 F1
-	texte = texte.replace(/ò/g,'&ograve;'); // 242 F2
-	texte = texte.replace(/ó/g,'&oacute;'); // 243 F3
-	texte = texte.replace(/ô/g,'&ocirc;'); // 244 F4
-	texte = texte.replace(/õ/g,'&otilde;'); // 245 F5
-	texte = texte.replace(/ö/g,'&ouml;'); // 246 F6
-	texte = texte.replace(/÷/g,'&divide;'); // 247 F7
-	texte = texte.replace(/ø/g,'&oslash;'); // 248 F8
-	texte = texte.replace(/ù/g,'&ugrave;'); // 249 F9
-	texte = texte.replace(/ú/g,'&uacute;'); // 250 FA
-	texte = texte.replace(/û/g,'&ucirc;'); // 251 FB
-	texte = texte.replace(/ü/g,'&uuml;'); // 252 FC
-	texte = texte.replace(/ý/g,'&yacute;'); // 253 FD
-	texte = texte.replace(/þ/g,'&thorn;'); // 254 FE
-	texte = texte.replace(/ÿ/g,'&yuml;'); // 255 FF
+	texte = texte.replace(/Ëœ/g,'&tilde;'); // 152 98
+	texte = texte.replace(/â„¢/g,'&trade;'); // 153 99
+	texte = texte.replace(/Å¡/g,'&scaron;'); // 154 9A
+	texte = texte.replace(/â€º/g,'&rsaquo;'); // 155 9B
+	texte = texte.replace(/Å“/g,'&oelig;'); // 156 9C
+	texte = texte.replace(/Å¾/g,'&#382;'); // 158 9E
+	texte = texte.replace(/Å¸/g,'&Yuml;'); // 159 9F
+	texte = texte.replace(/Â¡/g,'&iexcl;'); // 161 A1
+	texte = texte.replace(/Â¢/g,'&cent;'); // 162 A2
+	texte = texte.replace(/Â£/g,'&pound;'); // 163 A3
+	texte = texte.replace(/Â¥/g,'&yen;'); // 165 A5
+	texte = texte.replace(/Â¦/g,'&brvbar;'); // 166 A6
+	texte = texte.replace(/Â§/g,'&sect;'); // 167 A7
+	texte = texte.replace(/Â¨/g,'&uml;'); // 168 A8
+	texte = texte.replace(/Â©/g,'&copy;'); // 169 A9
+	texte = texte.replace(/Âª/g,'&ordf;'); // 170 AA
+	texte = texte.replace(/Â«/g,'&laquo;'); // 171 AB
+	texte = texte.replace(/Â¬/g,'&not;'); // 172 AC
+	texte = texte.replace(/Â­/g,'&shy;'); // 173 AD
+	texte = texte.replace(/Â®/g,'&reg;'); // 174 AE
+	texte = texte.replace(/Â¯/g,'&macr;'); // 175 AF
+	texte = texte.replace(/Â°/g,'&deg;'); // 176 B0
+	texte = texte.replace(/Â±/g,'&plusmn;'); // 177 B1
+	texte = texte.replace(/Â²/g,'&sup2;'); // 178 B2
+	texte = texte.replace(/Â³/g,'&sup3;'); // 179 B3
+	texte = texte.replace(/Â´/g,'&acute;'); // 180 B4
+	texte = texte.replace(/Âµ/g,'&micro;'); // 181 B5
+	texte = texte.replace(/Â¶/g,'&para'); // 182 B6
+	texte = texte.replace(/Â·/g,'&middot;'); // 183 B7
+	texte = texte.replace(/Â¸/g,'&cedil;'); // 184 B8
+	texte = texte.replace(/Â¹/g,'&sup1;'); // 185 B9
+	texte = texte.replace(/Âº/g,'&ordm;'); // 186 BA
+	texte = texte.replace(/Â»/g,'&raquo;'); // 187 BB
+	texte = texte.replace(/Â¼/g,'&frac14;'); // 188 BC
+	texte = texte.replace(/Â½/g,'&frac12;'); // 189 BD
+	texte = texte.replace(/Â¾/g,'&frac34;'); // 190 BE
+	texte = texte.replace(/Â¿/g,'&iquest;'); // 191 BF
+	texte = texte.replace(/Ã€/g,'&Agrave;'); // 192 C0
+	texte = texte.replace(/Ã�/g,'&Aacute;'); // 193 C1
+	texte = texte.replace(/Ã‚/g,'&Acirc;'); // 194 C2
+	texte = texte.replace(/Ãƒ/g,'&Atilde;'); // 195 C3
+	texte = texte.replace(/Ã„/g,'&Auml;'); // 196 C4
+	texte = texte.replace(/Ã…/g,'&Aring;'); // 197 C5
+	texte = texte.replace(/Ã†/g,'&AElig;'); // 198 C6
+	texte = texte.replace(/Ã‡/g,'&Ccedil;'); // 199 C7
+	texte = texte.replace(/Ãˆ/g,'&Egrave;'); // 200 C8
+	texte = texte.replace(/Ã‰/g,'&Eacute;'); // 201 C9
+	texte = texte.replace(/ÃŠ/g,'&Ecirc;'); // 202 CA
+	texte = texte.replace(/Ã‹/g,'&Euml;'); // 203 CB
+	texte = texte.replace(/ÃŒ/g,'&Igrave;'); // 204 CC
+	texte = texte.replace(/Ã�/g,'&Iacute;'); // 205 CD
+	texte = texte.replace(/ÃŽ/g,'&Icirc;'); // 206 CE
+	texte = texte.replace(/Ã�/g,'&Iuml;'); // 207 CF
+	texte = texte.replace(/Ã�/g,'&ETH;'); // 208 D0
+	texte = texte.replace(/Ã‘/g,'&Ntilde;'); // 209 D1
+	texte = texte.replace(/Ã’/g,'&Ograve;'); // 210 D2
+	texte = texte.replace(/Ã“/g,'&Oacute;'); // 211 D3
+	texte = texte.replace(/Ã”/g,'&Ocirc;'); // 212 D4
+	texte = texte.replace(/Ã•/g,'&Otilde;'); // 213 D5
+	texte = texte.replace(/Ã–/g,'&Ouml;'); // 214 D6
+	texte = texte.replace(/Ã—/g,'&times;'); // 215 D7
+	texte = texte.replace(/Ã˜/g,'&Oslash;'); // 216 D8
+	texte = texte.replace(/Ã™/g,'&Ugrave;'); // 217 D9
+	texte = texte.replace(/Ãš/g,'&Uacute;'); // 218 DA
+	texte = texte.replace(/Ã›/g,'&Ucirc;'); // 219 DB
+	texte = texte.replace(/Ãœ/g,'&Uuml;'); // 220 DC
+	texte = texte.replace(/Ã�/g,'&Yacute;'); // 221 DD
+	texte = texte.replace(/Ãž/g,'&THORN;'); // 222 DE
+	texte = texte.replace(/ÃŸ/g,'&szlig;'); // 223 DF
+	texte = texte.replace(/Ã /g,'&agrave;'); // 224 E0
+	texte = texte.replace(/Ã¡/g,'&aacute;'); // 225 E1
+	texte = texte.replace(/Ã¢/g,'&acirc;'); // 226 E2
+	texte = texte.replace(/Ã£/g,'&atilde;'); // 227 E3
+	texte = texte.replace(/Ã¤/g,'&auml;'); // 228 E4
+	texte = texte.replace(/Ã¥/g,'&aring;'); // 229 E5
+	texte = texte.replace(/Ã¦/g,'&aelig;'); // 230 E6
+	texte = texte.replace(/Ã§/g,'&ccedil;'); // 231 E7
+	texte = texte.replace(/Ã¨/g,'&egrave;'); // 232 E8
+	texte = texte.replace(/Ã©/g,'&eacute;'); // 233 E9
+	texte = texte.replace(/Ãª/g,'&ecirc;'); // 234 EA
+	texte = texte.replace(/Ã«/g,'&euml;'); // 235 EB
+	texte = texte.replace(/Ã¬/g,'&igrave;'); // 236 EC
+	texte = texte.replace(/Ã­/g,'&iacute;'); // 237 ED
+	texte = texte.replace(/Ã®/g,'&icirc;'); // 238 EE
+	texte = texte.replace(/Ã¯/g,'&iuml;'); // 239 EF
+	texte = texte.replace(/Ã°/g,'&eth;'); // 240 F0
+	texte = texte.replace(/Ã±/g,'&ntilde;'); // 241 F1
+	texte = texte.replace(/Ã²/g,'&ograve;'); // 242 F2
+	texte = texte.replace(/Ã³/g,'&oacute;'); // 243 F3
+	texte = texte.replace(/Ã´/g,'&ocirc;'); // 244 F4
+	texte = texte.replace(/Ãµ/g,'&otilde;'); // 245 F5
+	texte = texte.replace(/Ã¶/g,'&ouml;'); // 246 F6
+	texte = texte.replace(/Ã·/g,'&divide;'); // 247 F7
+	texte = texte.replace(/Ã¸/g,'&oslash;'); // 248 F8
+	texte = texte.replace(/Ã¹/g,'&ugrave;'); // 249 F9
+	texte = texte.replace(/Ãº/g,'&uacute;'); // 250 FA
+	texte = texte.replace(/Ã»/g,'&ucirc;'); // 251 FB
+	texte = texte.replace(/Ã¼/g,'&uuml;'); // 252 FC
+	texte = texte.replace(/Ã½/g,'&yacute;'); // 253 FD
+	texte = texte.replace(/Ã¾/g,'&thorn;'); // 254 FE
+	texte = texte.replace(/Ã¿/g,'&yuml;'); // 255 FF
 	return texte;
 	}
